08580d2a084189f55f0224850c04e1ca85c5e90c
VB <vb@Apple-MacBook-Pro.local>
2017-02-09 12:21:58 +0300

    Web Service Get
    Basit şekilde Visual Studio for Mac kullanarak web servis işlemleri

---
+++ WebApiAndroid/Controllers/OgrenciController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebApiAndroid.Controllers
+{
+    public class OgrenciController : ApiController
+    {
+		//normal zamanda entity fremawork kullanarak bağlamalıyız
+		//fakat macta onu tam çözemedim daha şimdilik custom bir listei ile bağlıyoruz
+
+		private List<Ogrenci> TumKayitlar()
+		{
+			List<Ogrenci> result = new List<Ogrenci>();
+
+			for (int i = 0; i < 10; i++)
+			{
+				result.Add(new Ogrenci() { id = i + 1, age = i + 20, name = "VB" + i, school = "SAU" + i });
+
+			}
+			return result;
+			//listemiz tamam şimdi web servisimizin get metodunu tamamlıyoruz
+		}
+
+		public HttpResponseMessage Get()
+		{
+			var ogrencilist = TumKayitlar();
+			return Request.CreateResponse(HttpStatusCode.OK, new { ogrenciler = ogrencilist });
+			//artik kayitlarımızı eklediğimiz ve json formatında çağırdıgımız kayıtlar hazır şimdi deneyelim
+			//kayitlarımız json formatında elimizde artık android cihazımızdan bu kayıtları kullanma zamanı
+
+		}
+    }
+}
---
+++ WebApiAndroid/Models/Ogrenci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace WebApiAndroid
+{
+	public class Ogrenci
+	{
+
+		//once ogrenci modelimizi yazıyoruz
+		//prop yazıp iki tab yapınca kısa yoldan ekliyoruz 
+		//şimdi controllerimizi yazalım
+
+		public int id
+		{
+			get;
+			set;
+		}
+		public string name
+		{
+			get;
+			set;
+		}
+
+		public int age
+		{
+			get;
+			set;
+		}
+		public string school
+		{
+			get;
+			set;
+		}
+
+	}
+}
